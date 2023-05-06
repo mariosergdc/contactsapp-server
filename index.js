4d0f675ea2c1d2ada7394950a7baeeb2fa147a17
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-05-06 12:18:45 -0400

    add cors origin

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ app.use(express.json({ limit: '50mb' }));
 //app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://contactsapp-client.onrender.com',
+    ],
+    credentials: true,
+  })
+);
 
 //credentials: true para permitir que mande cookies
 
